86ede3c85d080db88f085433b55ee876049b592c
Boris Matešin <boris.matesin@gmail.com>
2024-03-12 15:48:46 +0100

    Create gray_install_docker.sh

---
+++ gray_install_docker.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+install_arch () {
+    # Update repositories
+    pacman -Sy archlinux-keyring
+    pacman -Su
+
+    # Install Docker
+    pacman -S docker docker-compose docker-buildx
+
+    # Enable-start daemon
+    systemctl start docker.service
+    systemctl enable docker.service
+
+    # Add current user to group
+    gpasswd -a pyzeradmin docker
+}
+
+install_debian () {
+    # Add Docker's official GPG key:
+    apt-get update
+    apt-get install ca-certificates curl
+    install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+    chmod a+r /etc/apt/keyrings/docker.asc
+
+    # Add the repository to Apt sources:
+    echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+        $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+        tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    # Update repositories
+    apt-get update
+
+    # Install Docker
+    apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+}
+
+# No arguments for you!
+if [ $# != 0 ]; then
+    echo -e "Usage: $0\n"
+    exit 1
+fi
+
+# Run as root
+if [ "$EUID" -ne 0 ]; then
+    sudo "$(readlink -f $0)" "$@"
+    exit
+fi
+
+if [ $(grep ^NAME /etc/os-release | grep "Arch" | wc -l) -eq 1 ]; then
+    echo "-- Docker: Arch detected --"
+    install_arch
+    echo "Please logout and login before continuing installation."
+    exit 2
+elif [ $(grep ^NAME /etc/os-release | grep "Debian" | wc -l) -eq 1 ]; then
+    echo "-- Docker: Debian detected --"
+    install_debian
+    exit 0
+else
+    echo "-- Docker: distribution currently not supported --"
+    exit 3
+fi
